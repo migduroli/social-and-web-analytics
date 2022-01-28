387993322d59a11d18f352892d46db0043bbea05
Miguel A. Duran-Olivencia <migduroli@gmail.com>
2022-01-28 15:51:02 +0100

    :sparkles: add youtube example

---
+++ 02-social-data-collection/youtube/youtube_api.py
@@ -0,0 +1,82 @@
+import json
+import googleapiclient.discovery
+
+# API information
+api_service_name = "youtube"
+api_version = "v3"
+
+# API key
+with open("auth/youtube_credentials.json") as file:
+    credentials = json.load(file)
+
+DEVELOPER_KEY = credentials["DEVELOPER_KEY"]
+
+
+# API client
+youtube = googleapiclient.discovery.build(
+    serviceName=api_service_name,
+    version=api_version,
+    developerKey=DEVELOPER_KEY
+)
+
+# Request body:
+request = youtube.search().list(
+    part="id,snippet",
+    type='video',
+    q="Spider-Man",
+    videoDuration='short',
+    videoDefinition='high',
+    maxResults=10
+)
+
+# Request execution:
+response = request.execute()
+print(response)
+
+
+# Video by ID:
+video_id = "jg0H9uvZa-c"
+
+request = youtube.videos().list(
+    part="contentDetails,id,liveStreamingDetails,localizations,player,"
+         "recordingDetails,snippet,statistics,status,"
+         "topicDetails",
+    id=video_id
+).execute()
+
+# Comments in video:
+token = None
+comments = []
+for i in range(1,8):
+    request = youtube.commentThreads().list(
+        part="snippet",
+        videoId=video_id,
+        order="relevance",
+        pageToken=token,
+        maxResults=100,
+    ).execute()
+    token = request["nextPageToken"]
+    comments += request["items"]
+
+request = youtube.channels().list(
+    part="id,snippet,statistics",
+    forUsername="elrubiusOMG"
+).execute()
+
+
+YOUTUBERS = [
+    "elrubiusOMG",
+    "VEGETTA777",
+    "Mikecrack",
+    "AuronPlay",
+    "TheWillyRex",
+    "Makiman131",
+]
+
+for user_name in YOUTUBERS:
+    data = youtube.channels().list(
+        part="id,snippet,statistics",
+        forUsername="elrubiusOMG"
+    ).execute()["items"]
+
+    channel_data = data["items"][0]
