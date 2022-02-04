0b39ceab86c22260d31095ee3e5b0df5451d1baf
Miguel A. Duran-Olivencia <migduroli@gmail.com>
2022-02-04 16:48:22 +0100

    :sparkles: add 03-twitter.ipynb

--- 02-social-data-collection/facebook/fb_scrapper.py
+++ 02-social-data-collection/facebook/fb_scrapper.py
@@ -1,9 +1,15 @@
+import json
 import facebook_scraper as fb
 
 google_page = fb.get_page_info(account="Google")
+
+with open("auth/fb_cookies.json") as f:
+    cookies = json.load(f)
+
+fb.set_cookies(cookies)
 
 # Try to get all:
 for p in fb.get_posts("Google"):
     print(p)
 
-fb.get_profile("JOEROGAN")
+fb.get_profile("JOEROGAN", friends=True)
